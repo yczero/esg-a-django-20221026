41ea32663cd40b140193f6a8a2ee0d2e632dadc4
yczero <yc4038@naver.com>
2022-10-26 17:27:32 +0900

    177까지

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+def index(request):
+
+    post_qs = Post.objects.all().order_by('-id')
+    return render(request, 'blog/index.html', {
+        'post_list' : post_qs, } )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk) # 값 그자체 keyword??
+    return render(request, 'blog/single_post_page.html',{ 'post' : post ,})
+    
--- do_it_django_prj1026/urls.py
+++ do_it_django_prj1026/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.http import HttpResponse
+
+
+def root(request):
+    return HttpResponse('hello django')
+
 
 urlpatterns = [
+    path('', root),
+    path('blog/', include('blog.urls')),
     path("admin/", admin.site.urls),
 ]
